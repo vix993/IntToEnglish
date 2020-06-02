fb883c345dbff001ff4381d1c2362cc0e29c339e
Victor <victor.as.nascimento@gmail.com>
2020-06-02 13:09:39 -0300

    fix no arg error

--- test.c
+++ test.c
@@ -6,6 +6,8 @@ char	*intToEnglish(int number);
 
 int	main(int argc, char **argv)
 {
+	if (!argv[1])
+		argv[1] = "42";
 	printf("%s = %s\n", argv[1], intToEnglish(atoi(argv[1])));
 	printf("%d = %s\n", INT_MAX, intToEnglish(INT_MAX));
 	printf("%d = %s\n", INT_MIN + 1, intToEnglish(INT_MIN + 1));
